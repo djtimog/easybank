559cf75b5b49cbdbcc21316fc36a5dcb5efc30e2
djtimog <ogunleyetimilehin15@gmail.com>
2024-09-19 14:53:39 +0100

    starting header added necessary nav links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
 export default function Header() {
     return(
         <>
             <header>
-                <h1>Header</h1>
+                <nav>
+                    <div>
+                        <Link href={"/"}>easy bank</Link>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><Link href={"/"}>Home</Link></li>
+                            <li><Link href={"/"}>About</Link></li>
+                            <li><Link href={"/"}>Contact</Link></li>
+                            <li><Link href={"/"}>Blogs</Link></li>
+                            <li><Link href={"/"}>Careers</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <Link href={"/"}>Request Invite</Link>
+                    </div>
+                </nav>
             </header>
         </>
     )
