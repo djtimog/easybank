1b3a0f114ae697cae72d5dd3a01e6a8a8cf6f49e
djtimog <ogunleyetimilehin15@gmail.com>
2024-09-19 19:55:56 +0100

    done with header for desktop view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,37 @@
 import Link from "next/link";
+import logo from "@/images/logo.svg"
+import Image from "next/image";
 export default function Header() {
     return(
         <>
-            <header>
-                <nav className="d-flex justify-content-around align-content-center">
-                    <div>
-                        <Link href={"/"} className="nav-link">easy bank</Link>
+            <header className="py-3 position-sticky w-100">
+                <nav className="d-flex justify-content-around">
+                    <div className="align-self-center">
+                        <Link href={"/"} className="nav-link align-self-center">
+                            <Image src={logo.src} alt="easy bank logo" className="img-fluid w-100" width={`1`} height={`1`} />
+                        </Link>
                     </div>
-                    <div className="d-none d-md-block">
+                    <div className="d-none d-md-block align-self-center">
                         <ul className="nav">
-                            <li className="nav-item disabled">
-                                <Link href={"/"} className="nav-link" >Home</Link>
+                            <li className="nav-item">
+                                <Link href={"/"} className="nav-link text-secondary">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link href={"/"} className="nav-link">About</Link>
+                                <Link href={"/"} className="nav-link text-secondary">About</Link>
                             </li>
                             <li className="nav-item">
-                                <Link href={"/"} className="nav-link">Contact</Link>
+                                <Link href={"/"} className="nav-link text-secondary">Contact</Link>
                             </li>
                             <li className="nav-item">
-                                <Link href={"/"} className="nav-link">Blogs</Link>
+                                <Link href={"/"} className="nav-link text-secondary">Blogs</Link>
                             </li>
                             <li className="nav-item">
-                                <Link href={"/"} className="nav-link">Careers</Link>
+                                <Link href={"/"} className="nav-link text-secondary">Careers</Link>
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <Link href={"/"} className="nav-link">Request Invite</Link>
+                    <div className="align-self-center">
+                        <Link href={"/"} className="nav-link px-5 py-3 bg-primary-gradient rounded-pill text-white">Request Invite</Link>
                     </div>
                 </nav>
             </header>
